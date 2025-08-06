9b9b02b501a067057758e654bc6e308a1b6b10fa
elitestack <favourstack@gmail.com>
2025-08-06 10:20:33 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -710,16 +710,16 @@ app.post('/api/withdraw', authenticateToken, async (req, res) => {
       });
     }
 
-      const userEmail = req.user.email;
+//       const userEmail = req.user.email;
 
-    // Special rule for test@test.test1
-  if (userEmail.toLowerCase() === 'mariusvinkelis@gmail.com') {
-  if (String(amount) !== '$10') {
-    return res.status(403).json({
-      message: 'You have passed your withdrawal limit'
-    });
-  }
-}
+//     // Special rule for test@test.test1
+//   if (userEmail.toLowerCase() === 'mariusvinkelis@gmail.com') {
+//   if (String(amount) !== '$10') {
+//     return res.status(403).json({
+//       message: 'You have passed your withdrawal limit'
+//     });
+//   }
+// }
 
     
     // No numeric validation or balance check — we accept it as-is
